932210c7ed3d5c7c4371e0b80a303898ec362fc7
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-15 20:38:20 +0100

    add dj_database

--- ink_dream/settings.py
+++ ink_dream/settings.py
@@ -2,6 +2,7 @@
 import os
 # import env
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,13 +79,21 @@ WSGI_APPLICATION = 'ink_dream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# if DEBUG:
+    
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+    
+#     DATABASE_URL = os.environ['DATABASE_URL']
 
+    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
