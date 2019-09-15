e65d2520754541e5148681e79bc3f3e4fa5b50ca
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-15 21:00:33 +0100

    add migrations

--- ink_dream/settings.py
+++ ink_dream/settings.py
@@ -80,7 +80,6 @@ WSGI_APPLICATION = 'ink_dream.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-    
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
