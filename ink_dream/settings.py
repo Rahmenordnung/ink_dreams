f90be725b85573c649b0abef5f3f931214f867ef
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-15 20:40:35 +0100

    add dj_database

--- ink_dream/settings.py
+++ ink_dream/settings.py
@@ -79,19 +79,14 @@ WSGI_APPLICATION = 'ink_dream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# if DEBUG:
-    
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-    
-#     DATABASE_URL = os.environ['DATABASE_URL']
 
-    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
