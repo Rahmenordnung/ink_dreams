8321739fc31910c6ed765c7d8c76d6ca13a9c2d5
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-22 14:00:12 +0100

    update Readme, travis

---
+++ env.py
@@ -0,0 +1,15 @@
+import os
+
+#stripe
+os.environ.setdefault('STRIPE_PUBLISHABLE', "pk_test_xw1BD4o3yGettsXTTOxuOrAM00k7B6gTMb")
+os.environ.setdefault('STRIPE_SECRET', "sk_test_R1Vl3skEd4ahbpxKJaXo8a3a00LKvKkGQm")
+#databases
+os.environ.setdefault("DATABASE_URL", "postgres://pgfmbrgvuqoess:cdf0a3103979277263a6ae1f3679f0aaac0f686e521e8fb6d39fab7dccbc7183@ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/duumheqafm1bd")
+os.environ.setdefault("SECRET_KEY", "$-t*iczx5ohb=%sjbx^9rrb(hq72m-5ng)0#4qhzfie@*+d4^1")
+
+
+#email smtp
+os.environ['EMAIL_USER'] = 'othdur@gmail.com'
+os.environ['EMAIL_PASSWORD'] = 'Uniform1'
+
+
