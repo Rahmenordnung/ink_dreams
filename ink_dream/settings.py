756f34dc3c2808f233b52bf818310d2792a11e78
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-24 22:48:55 +0100

    settings adjusted to heroku

--- ink_dream/settings.py
+++ ink_dream/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ink-dreams-app.herokuapp.com']
+ALLOWED_HOSTS = ['ink-dreams.herokuapp.com']  
 
 
 # Application definition
