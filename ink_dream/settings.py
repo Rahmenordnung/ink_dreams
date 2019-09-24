c9cbe60be721174d132a0d23737c7e708b577e94
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-24 22:44:19 +0100

    settings adjusted

--- ink_dream/settings.py
+++ ink_dream/settings.py
@@ -1,14 +1,12 @@
 
 import os
-import env
-import dj_database_url
-
+# import env
 # import dj_database_url
 
 # if os.path.exists('env.py'):
 #     import env
-# import django_heroku
-# import dj_database_url
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://ink-dreams-app.herokuapp.com/']
+ALLOWED_HOSTS = ['ink-dreams-app.herokuapp.com']
 
 
 # Application definition
@@ -88,15 +86,20 @@ WSGI_APPLICATION = 'ink_dream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-else:
-    print("Database URL not found. Using SQLite instead")
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+# else:
+#     print("Database URL not found. Using SQLite instead")
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Heoku Postgresql database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
