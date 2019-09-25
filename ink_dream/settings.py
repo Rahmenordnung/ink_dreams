df7d958c7f1967e265e23a6d0bdd984a63ba0283
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-09-25 12:18:56 +0100

    correcting app requirements versions for travis

--- ink_dream/settings.py
+++ ink_dream/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$-t*iczx5ohb=%sjbx^9rrb(hq72m-5ng)0#4qhzfie@*+d4^1'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
